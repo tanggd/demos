73156c3663e2b130547f43b1bcba0c1039578249
tanggd <tanggd11@foxmail.com>
2021-08-04 10:29:49 +0800

    feat: service worker

--- crossTabCommunication/sw.js
+++ crossTabCommunication/sw.js
@@ -1,14 +1,8 @@
-
 self.addEventListener('message', (e) => {
-  console.log('eee', e)
   e.waitUntil(
     // 通过self.clients.matchAll()获取注册了该 Service Worker 的所有同源页面
     self.clients.matchAll().then((clients) => {
-      if (!clients || clients.length === 0) {
-        return
-      }
       clients.forEach(function (client) {
-        console.log('client', client)
         client.postMessage(e.data)
       })
     })
